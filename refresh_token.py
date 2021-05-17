ba7e640abc32fac622f4612d5e2de639a40f119e
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-05-17 15:30:23 -0500

    Varias pruebas

---
+++ refresh_token.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_refresh_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import unset_jwt_cookies
+
+app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(seconds=10)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=5)
+jwt = JWTManager(app)
+
+
+# We verify the users password here, so we are returning a fresh access token
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity="example_user", fresh=True)
+    refresh_token = create_refresh_token(identity="example_user")
+    return jsonify(access_token=access_token, refresh_token=refresh_token)
+
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    logout = unset_jwt_cookies(response)
+    print(logout)
+    return response
+
+
+# If we are refreshing a token here we have not verified the users password in
+# a while, so mark the newly created access token as not fresh
+@app.route("/refresh", methods=["GET"])
+@jwt_required(refresh=True)
+def refresh():
+    identity = get_jwt_identity()
+    access_token = create_access_token(identity=identity, fresh=False)
+    return jsonify(access_token=access_token)
+
+
+# Only allow fresh JWTs to access this route with the `fresh=True` arguement.
+@app.route("/protected", methods=["GET"])
+@jwt_required(fresh=True)
+def protected():
+    return jsonify(foo="bar")
+
+
+@app.route("/ambos", methods=["GET"])
+@jwt_required(fresh=False, refresh=False)
+def ambos():
+    return jsonify(valor="ambos")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
