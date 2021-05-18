33aa63c9164e0bda7474f60b544221ec5475b91b
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-05-17 22:14:53 -0500

    Haciendo pruebas con refresh cookie

--- refresh_token.py
+++ refresh_token.py
@@ -1,6 +1,4 @@
-from datetime import datetime
 from datetime import timedelta
-from datetime import timezone
 
 from flask import Flask
 from flask import jsonify
@@ -11,13 +9,12 @@ from flask_jwt_extended import create_refresh_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
-from flask_jwt_extended import unset_jwt_cookies
 
 app = Flask(__name__)
 
 app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(seconds=10)
-app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=5)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 jwt = JWTManager(app)
 
 
@@ -26,25 +23,19 @@ jwt = JWTManager(app)
 def login():
     username = request.form["username"]
     password = request.form["password"]
+    
     if username != "test" or password != "test":
         return jsonify({"msg": "Bad username or password"}), 401
 
     access_token = create_access_token(identity="example_user", fresh=True)
     refresh_token = create_refresh_token(identity="example_user")
+
     return jsonify(access_token=access_token, refresh_token=refresh_token)
-
-
-@app.route("/logout", methods=["POST"])
-def logout():
-    response = jsonify({"msg": "logout successful"})
-    logout = unset_jwt_cookies(response)
-    print(logout)
-    return response
 
 
 # If we are refreshing a token here we have not verified the users password in
 # a while, so mark the newly created access token as not fresh
-@app.route("/refresh", methods=["GET"])
+@app.route("/refresh", methods=["POST"])
 @jwt_required(refresh=True)
 def refresh():
     identity = get_jwt_identity()
@@ -52,6 +43,7 @@ def refresh():
     return jsonify(access_token=access_token)
 
 
+# fresh=True se podria ocupar cuando el usuario tenga que hacer una accion con limite de tiempo, como una transferencia o un pago online.
 # Only allow fresh JWTs to access this route with the `fresh=True` arguement.
 @app.route("/protected", methods=["GET"])
 @jwt_required(fresh=True)
@@ -59,10 +51,22 @@ def protected():
     return jsonify(foo="bar")
 
 
-@app.route("/ambos", methods=["GET"])
-@jwt_required(fresh=False, refresh=False)
-def ambos():
-    return jsonify(valor="ambos")
+@app.route("/contacto", methods=["GET"])
+@jwt_required(fresh=False)
+def contacto():
+    return jsonify(contacto="OK contacto")
+
+
+@app.route("/servicio", methods=["GET"])
+@jwt_required(fresh=False)
+def servicio():
+    return jsonify(servicio="OK servicio")
+
+
+@app.route("/ticket", methods=["GET"])
+@jwt_required(fresh=False)
+def ticket():
+    return jsonify(ticket="OK ticket")
 
 
 if __name__ == "__main__":
